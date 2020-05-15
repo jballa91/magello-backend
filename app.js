bbb0dba37093c8adb2a1be14c9c70b859f17602a
James Ballard <jamesballard901@gmail.com>
2020-05-15 12:27:08 -0500

    Fix cors origin

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const cardsRouter = require("./routes/cards");
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
